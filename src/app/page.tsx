d17d03b34eb71eb08a5d5f88ffda2a94bdbb6e63
daffa <hanura423@gmail.com>
2025-05-15 17:14:49 +0700

    isView : Tambah usestate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,56 @@
+"use client";
+import ViewData from "@/components/ViewData";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [isView, setIsView] = useState(false);
+  const handleView = () => {
+    setIsView(!isView);
+  };
   return (
-    <section className="bg-gray-900 flex items-center justify-center min-h-screen">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
-          We invest in the world’s potential
-        </h1>
-        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-          Here at Flowbite we focus on markets where technology, innovation, and
-          capital can unlock long-term value and drive economic growth.
-        </p>
-        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-          <Link
-            href="/students"
-            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-          >
-            Get started
-            <svg
-              className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
+    <>
+      <section className="bg-gray-900 flex items-center justify-center min-h-screen">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
+            We invest in the world’s potential
+          </h1>
+          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+            Here at Flowbite we focus on markets where technology, innovation,
+            and capital can unlock long-term value and drive economic growth.
+          </p>
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <Link
+              href="/students"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </Link>
-          <a
-            href="#"
-            className="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-          >
-            View Data
-          </a>
+              Get started
+              <svg
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+            <button
+              onClick={handleView}
+              className="py-3 font-semibold cursor-pointer px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              View Data
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section id="ViewData">{isView && <ViewData />}</section>
+    </>
   );
 }
--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,23 +1,17 @@
 import FormInput from "@/components/FormInput";
-import ViewData from "@/components/ViewData";
 import React from "react";
 
 export default function Students() {
   return (
-    <>
-      <section className="min-h-screen bg-[#0E1E27]">
-        <div className="flex  container  mx-auto flex-col md:flex-row items-center justify-center min-h-screen gap-5">
-          <div className="max-w-md  mx-auto">
-            <h1 className="text-[2rem] md:text-[3rem] lg:text-[6rem] text-[#00A6FB]  font-bold">
-              Frosti High School
-            </h1>
-          </div>
-          <FormInput />
+    <section className="min-h-screen bg-[#0E1E27]">
+      <div className="flex  container  mx-auto flex-col md:flex-row items-center justify-center min-h-screen gap-5">
+        <div className="max-w-md  mx-auto">
+          <h1 className="text-[2rem] md:text-[3rem] lg:text-[6rem] text-[#00A6FB]  font-bold">
+            Frosti High School
+          </h1>
         </div>
-      </section>
-      <section id="ViewData">
-        <ViewData />
-      </section>
-    </>
+        <FormInput />
+      </div>
+    </section>
   );
 }
